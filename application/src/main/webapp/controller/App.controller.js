c970fd3962fbe75b870e87f58a8e2ef48d5791c2
NYellapu <nagesh.yellapu@mckesson.eu>
2018-12-01 01:38:26 +0100

    W1U5

--- application/src/main/webapp/controller/App.controller.js
+++ application/src/main/webapp/controller/App.controller.js
@@ -8,7 +8,14 @@ sap.ui.define([
 
 		onShowHello : function () {
 			// show a native JavaScript alert
-			MessageToast.show("Hello openSAP");
+			//MessageToast.show("Hello openSAP");
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
+
 		}
 	});
 
