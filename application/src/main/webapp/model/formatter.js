8e60ac1c45f4e66a8bf08d9a9be36ac8ce2a2faf
NYellapu <nagesh.yellapu@mckesson.eu>
2018-12-05 15:32:36 +0100

    W2U2

--- application/src/main/webapp/model/formatter.js
+++ application/src/main/webapp/model/formatter.js
@@ -2,19 +2,21 @@ sap.ui.define([], function(){
     "use strict";
 
     return {
-        delivery : function(iWeight,sMeasure){
+        delivery : function(sMeasure,iWeight){
             var sResult="";
 
+            var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
             if(sMeasure==="G"){
                 iWeight = iWeight/1000;
             }
-            if(iWeight<0.5){
-                sResult="Mail Deliver";
-            } else if(iWeight<5){
-                sResult="Parcel Deliver";
-            } else {
-                sResult="Carrier Deliver";
-            }
+            if (iWeight < 0.5) {
+				sResult = oResourceBundle.getText("formatterMailDelivery");
+			} else if (iWeight < 5) {
+				sResult = oResourceBundle.getText("formatterParcelDelivery");
+			} else {
+				sResult = oResourceBundle.getText("formatterCarrierDelivery");
+			}
+
 
             return sResult;
         }
